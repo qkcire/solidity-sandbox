b77cc694f2521a200cc37f6341333744072a5496
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-04 01:21:51 -0700

    Add bid method

--- solidity_docs/SimpleAuction.sol
+++ solidity_docs/SimpleAuction.sol
@@ -46,4 +46,44 @@ contract SimpleAuction {
     auctionEndTime = block.timestamp + biddingTime;
   }
   
+  /// Bid on the auction with the value sent
+  /// together with this transaction.
+  /// The value will only be refunded if the
+  // auction is not won.
+  function bid() external payable {
+    // No arguments are necessary, all
+    // information is already part of
+    // the transaction. The keyword payable
+    // is required for the function to
+    // be able to receive Ether.
+
+    // Revert the call if the bidding
+    // period is over.
+    if (block.timestamp > auctionEndTime) {
+      revert AuctionAlreadyEnded();
+    }
+
+    // If the bid if not higher, send the
+    // money back (the revert statement
+    // will revert all the changes in this
+    // function execution including 
+    // it having received the money).
+    if (msg.value <= highestBid) {
+      rever BidNotHighEnough(highestBid);
+    }
+
+    if (highestBid != 0) {
+      // Sending back the money by simply using
+      // highestBidder.send(highestBid) is a 
+      // security risk because it could execute
+      // an untrusted contract.
+      // It is always safer to let the recipients
+      // withdraw their money themselves.
+      pendingReturns[highestBIdder] += highestBid;
+    }
+    highestBIdder = msg.sender;
+    highestBid = msg.value;
+    emit HighestBidIcreased(msg.sender, msg.value);
+  }
+
 }
