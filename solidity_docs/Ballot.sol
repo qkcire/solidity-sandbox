047c1407d14f83c6d4f9b252729662b9a13aa905
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-04 00:42:19 -0700

    Add delegate method

--- solidity_docs/Ballot.sol
+++ solidity_docs/Ballot.sol
@@ -72,4 +72,21 @@ contract Ballot {
     voters[voter].weight = 1;
   }
 
+  /// Delegate your vote to the voter `to`.
+  function delegate(address to) external {
+    // Assigns reference
+    Voter storage sender = voters[msg.sender];
+    require(!sender.voted, "You already voted.");
+
+    require(to != msg.sender, "Self-delegation is disallowed.");
+
+    // Forward the delegation as long as
+    // `to` also delegated.
+    // In general, such loops are very dangerous,
+    // because if they run too long, they might
+    // need more gas than is available in a block.
+    // In this case, the delegation will not be executed,
+    //
+  }
+
 }
