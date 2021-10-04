4946571e57a3a8c3d133be0798733d25c44bfa7d
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-04 01:23:56 -0700

    Add withdraw method

--- solidity_docs/SimpleAuction.sol
+++ solidity_docs/SimpleAuction.sol
@@ -86,4 +86,22 @@ contract SimpleAuction {
     emit HighestBidIcreased(msg.sender, msg.value);
   }
 
+  /// Withdraw a bid that was overbid.
+  function withdraw() external returns (bool) {
+    uint amount = pendingReturns[msg.sender];
+    if (amount > 0) {
+      // It is important to set this to zero because
+      // the recipient can call this function again
+      // as part of the receiving call before `send`
+      // returns.
+      pendingReturns[msg.sender] = 0;
+
+      if (!payable(msg.sender).send(amount)) {
+        // No need to call throw here, just reset the amount owing
+        return false;
+      }
+    }
+    return true;
+  }
+
 }
